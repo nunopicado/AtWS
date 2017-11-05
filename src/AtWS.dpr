eb64de483aa131bba5d5e5e442ffd612f7607bef
Nuno Picado <geral@nunopicado.com>
2017-11-05 12:24:39 +0000

    Added modified libeay32 submodule

--- src/AtWS.dpr
+++ src/AtWS.dpr
@@ -26,7 +26,9 @@ library AtWS;
 uses
   AtXMLDocument in 'AtXMLDocument.pas',
   AtWSvc in 'AtWSvc.pas',
-  AtWSvcIntf in 'AtWSvcIntf.pas';
+  AtWSvcIntf in 'AtWSvcIntf.pas',
+  libeay32 in 'ThirdParty\libeay32\libeay32.pas',
+  TZDB in 'ThirdParty\ReusableObjects\src\ThirdParty\tzdb\src\TZDBPK\TZDB.pas';
 
 {$R *.res}
 
